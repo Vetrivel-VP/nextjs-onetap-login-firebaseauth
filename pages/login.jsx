81d18baadfdff87870d1c3dc22cdae72971cb4d2
VETRIVEL <vetrivel.galaxy@gmail.com>
2022-02-12 17:42:04 +0300

    Changes : Project Initialized

---
+++ pages/login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const login = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-slate-400">
+      login
+    </div>
+  );
+};
+
+export default login;
