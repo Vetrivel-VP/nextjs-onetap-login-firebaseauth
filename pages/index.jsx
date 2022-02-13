88c11ab553c3044f2332544680502a52a5e21bfe
VETRIVEL <vetrivel.galaxy@gmail.com>
2022-02-13 13:01:42 +0300

    Changes : Firebase Authentication Done

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { userAccessToken, fetchUser } from "../utils/fetchDetails";
+import { useRouter } from "next/router";
+import { IoLogOut } from "react-icons/io5";
 
 const index = () => {
+  const router = useRouter();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const accessToken = userAccessToken();
+    if (!accessToken) return router.push("/login");
+    const [userInfo] = fetchUser();
+    console.log(userInfo);
+    setUser(userInfo);
+  }, []);
+
+  const logout = () => {
+    localStorage.clear();
+    router.push("/login");
+  };
+
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-slate-400">
-      index
+    <div className="w-screen h-screen flex justify-center items-center bg-slate-100">
+      <div className="w-1/3 h-auto p-4 bg-white shadow-md rounded-md flex justify-start items-center relative">
+        <IoLogOut
+          fontSize={25}
+          className="absolute top-3 right-3 cursor-pointer text-gray-600"
+          onClick={logout}
+        />
+        <img src={user?.photoURL} className="rounded-md shadow-md" alt="" />
+        <p className="text-2xl font-sans font-semibold ml-2">
+          {user?.displayName}
+          <span className="block text-xs font-serif font-normal">
+            {user?.email}
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,17 +1,39 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { firebaseApp } from "../firebase-config";
+import { useRouter } from "next/router";
+
 const login = () => {
+  const firebaseAuth = getAuth(firebaseApp);
+  const provider = new GoogleAuthProvider();
+  const router = useRouter();
+
+  const signIn = async () => {
+    const { user } = await signInWithPopup(firebaseAuth, provider);
+    const { refreshToken, providerData } = user;
+    console.log(refreshToken, providerData);
+    localStorage.setItem("user", JSON.stringify(providerData));
+    localStorage.setItem("accessToken", JSON.stringify(refreshToken));
+    router.push("/");
+  };
+
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-white">
-      <div className="flex flex-col border items-center border-gray-300 rounded-sm w-[550px] p-4">
-        <div
-          className="flex justify-center items-center border border-gray-300 p-2 w-1/2 
-                    rounded-full cursor-pointer hover:shadow-md duration-150 ease-in-out"
-        >
-          <FcGoogle fontSize={30} />
-          <p className="text-lg font-semibold ml-4">Sign in with Google</p>
-        </div>
+    <div className="w-screen h-screen flex justify-center items-center bg-white relative">
+      <img
+        src="https://cdn.pixabay.com/photo/2016/06/11/12/13/pink-hair-1450045_960_720.jpg"
+        alt=""
+        className="absolute top-0 left-0 w-screen h-screen object-cover"
+      />
+      <div className="top-0 left-0 right-0 bottom-0 absolute bg-black opacity-50"></div>
+      <div
+        className="flex justify-center items-center border border-gray-300 p-2 bg-white bg-opacity-60 
+                    rounded-full cursor-pointer hover:shadow-md hover:bg-opacity-100 duration-150 ease-in-out z-10"
+        onClick={signIn}
+      >
+        <FcGoogle fontSize={30} />
+        <p className="text-lg font-semibold ml-4">Sign in with Google</p>
       </div>
     </div>
   );
